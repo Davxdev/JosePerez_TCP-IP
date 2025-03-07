3b587ebba31b99b076e6aa861232fa4e4451588f
Jose Perez <josedpg3@gmail.com>
2025-03-07 15:58:29 -0500

    First commit

---
+++ ClienteTcpImc/ClienteTcpImc/src/joseperez/imc/cliente/Principal.java
@@ -0,0 +1,23 @@
+package joseperez.imc.cliente;
+
+import joseperez.imc.cliente.vistas.VentanaPrincipal;
+/**
+ *
+ * @author Josxxsj
+ */
+public class Principal {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+
+
+        
+    }
+    
+}
---
+++ ServidorTcpImc/ServidorTcpImc/src/joseperez/imc/Principal.java
@@ -0,0 +1,19 @@
+
+package joseperez.imc;
+
+import joseperez.imc.vistas.VentanaPrincipal;
+/**
+ *
+ * @author Josxxsj
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
